14fc6d1a7ae7f76ac87ecae7a277da19630db4de
Priyanshu Agrawal <spiderfan.no1@gmail.com>
2024-02-17 12:50:19 +0530

    fix: flat list is breaking so due to internal glitch using flashlist instead in Home page

--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import blogCard from "../../components/blogComponents/blogCard";
 import {API_URL} from "../../constants";
+import { FlashList } from "@shopify/flash-list";
 
 
 const Home = ({navigation}) => {
@@ -17,7 +18,11 @@ const Home = ({navigation}) => {
 	}, []);
 	return (
 		<View className={'relative box-border bg-transparent h-full w-full py-2'}>
-			<FlatList
+			<FlashList data={blogs} renderItem={({item}) => blogCard({...item, navigation})} keyExtractor={item => item._id} estimatedItemSize={138} />
+			
+			
+			{/*Flat list is Breaking here*/}
+			{/*<FlatList
 				data={blogs}
 				keyExtractor={item => item._id}
 				renderItem={({item}) => blogCard({...item, navigation})}
@@ -26,7 +31,8 @@ const Home = ({navigation}) => {
 				maxToRenderPerBatch={5}
 				initialNumToRender={5}
 				windowSize={10}
-			/>
+			/>*/}
+			
 			<TouchableOpacity className={'bg-indigo-950 w-16 aspect-square  rounded-full items-center justify-center absolute  bottom-5 right-5'} onPress={()=> navigation.navigate('AddBlog')}>
 				<Text className={'text-white text-4xl'}>+</Text>
 			</TouchableOpacity>
